64234075579e762bfe2be8e0bd6e40fa6653a113
Nadun Perera <nadun.perera@me.com>
2019-05-15 20:27:50 +1000

    added product, price and cents to woolworths.py

--- ghn_crawler/spiders/coles.py
+++ ghn_crawler/spiders/coles.py
@@ -4,23 +4,43 @@ from scrapy_splash import SplashRequest
 
 
 class MySpider(scrapy.Spider):
-    name = "woolworths"
-    start_urls = ["https://www.woolworths.com.au/shop/browse/dairy-eggs-fridge"]
+    name = "coles"
+    start_urls = [
+        "https://shop.coles.com.au/a/a-national/everything/browse/entertaining-at-home/cheese-board-selections?pageNumber=1"
+    ]
+
+    lua_script = """
+    function main(splash, args)
+      assert(splash:go(args.url))
+      assert(splash:wait(0.5))
+      return {
+        html = splash:html(),
+        png = splash:png(),
+      }
+    end
+    """
 
     def start_requests(self):
         for url in self.start_urls:
-            # Uncomment below if you want a screenshot of the response
             # yield SplashRequest(url, self.parse, endpoint='render.json', args={"wait": 5, "png": 1, "render_all": 1})
-            yield SplashRequest(url, self.parse, args={"wait": 5})
+            # yield SplashRequest(url, self.parse, args={"wait": 5})
+            yield SplashRequest(
+                url,
+                self.parse,
+                endpoint="execute",
+                magic_response=True,
+                meta={"handle_httpstatus_all": True},
+                args={"lua_source": self.lua_script},
+            )
 
     def parse(self, response):
-        # Uses the product model in items.py
+        print("PRINTING BODY")
+        print(response.body)
         # item = Product()
         # for product in response.css('shelfProductTile'):
         #     item['product_name'] = product.css('shelfProductTile-description::text').extract_first()
         #     yield item
 
-        # Get screenshot of the output
         # imgdata = base64.b64decode(response.data['png'])
         # filename = "some_image.png"
         # with open(filename, "wb") as f:
